5a83d661d006712160fe726ee2e7b0921f7dac19
DariaMelnikova27 <54035863+DariaMelnikova27@users.noreply.github.com>
2022-11-29 22:39:18 +0300

    homework5

---
+++ conftest.py
@@ -0,0 +1,53 @@
+import os
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--driver_folder", default=os.path.expanduser("~/Downloads/drivers"))
+    parser.addoption("--url", action="store", default="http://192.168.0.101:8081")
+    parser.addoption("--status_code", default="200", type=int)
+
+
+@pytest.fixture
+def browser(request):
+    _browser = request.config.getoption("--browser")
+    driver_folder = request.config.getoption("--driver_folder")
+    driver = None
+    url = request.config.getoption("--url")
+
+    if _browser == "firefox" or _browser == "ff":
+        options = FirefoxOptions()
+        driver = webdriver.Firefox(
+            executable_path=f"{driver_folder}{os.sep}geckodriver", options=options
+        )
+    elif _browser == "chrome":
+        options = ChromeOptions()
+        driver = webdriver.Chrome(
+            options=options, executable_path=f"{driver_folder}{os.sep}chromedriver"
+        )
+    elif _browser == "edge":
+        options = EdgeOptions()
+        driver = webdriver.Edge(
+            executable_path=f"{driver_folder}{os.sep}msedgedriver", options=options
+        )
+    elif _browser == "yandex":
+        options = ChromeOptions()
+        driver = webdriver.Chrome(
+            executable_path=f"{driver_folder}{os.sep}yandexdriver", options=options
+        )
+    elif _browser == "safari":
+        driver = webdriver.Safari()
+
+    driver.maximize_window()
+
+    request.addfinalizer(driver.close)
+
+    driver.get(url)
+    driver.url = url
+
+    return driver
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,1 +1,56 @@
+from page_objects import GeneralPage, CatalogPage, ProductCardPage, AdminPage, AccountRegisterPage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+def test_general_page(browser):
+    browser.get(browser.url)
+    wait = WebDriverWait(browser, 3)
+    wait.until(EC.visibility_of_element_located(GeneralPage.WISHLIST))
+    browser.find_element(*GeneralPage.WISHLIST)
+    browser.find_element(*GeneralPage.LOGO)
+    browser.find_element(*GeneralPage.MACBOOK)
+    browser.find_element(*GeneralPage.SWIPER)
+    browser.find_element(*GeneralPage.ABOUT_US)
+
+
+def test_catalog_page(browser):
+    browser.get(browser.url + "/index.php?route=product/category&path=20")
+    browser.find_element(*CatalogPage.GRID_VIEW_BUTTON)
+    browser.find_element(*CatalogPage.LIST_VIEW_BUTTON)
+    wait = WebDriverWait(browser, 2)
+    wait.until(EC.visibility_of_element_located(CatalogPage.MENU))
+    browser.find_element(*CatalogPage.MENU)
+    browser.find_element(*CatalogPage.SORT_FIELD)
+    browser.find_element(*CatalogPage.BANNER)
+
+
+def test_product_card_page(browser):
+    browser.get(browser.url + "/tablet/samsung-galaxy-tab-10-1")
+    browser.find_element(*ProductCardPage.NAVBAR)
+    browser.find_element(*ProductCardPage.DESCRIPTION)
+    browser.find_element(*ProductCardPage.ADD_TO_CART)
+    browser.find_element(*ProductCardPage.REVIEWS)
+    browser.find_element(*ProductCardPage.TWEET)
+
+
+def test_admin_page(browser):
+    browser.get(browser.url + "/admin")
+    browser.find_element(*AdminPage.FOOTER)
+    wait = WebDriverWait(browser, 3)
+    wait.until(EC.visibility_of_element_located(AdminPage.FOOTER))
+    browser.find_element(*AdminPage.FORGOTTEN_PASSWORD)
+    browser.find_element(*AdminPage.INPUT_USERNAME)
+    browser.find_element(*AdminPage.INPUT_PASSWORD)
+    browser.find_element(*AdminPage.SUBMIT_BUTTON)
+
+
+def test_account_register_page(browser):
+    browser.get(browser.url + "/index.php?route=account/register")
+    browser.find_element(*AccountRegisterPage.PERSONAL_DETAILS)
+    browser.find_element(*AccountRegisterPage.INPUT_FIRSTNAME)
+    wait = WebDriverWait(browser, 1)
+    wait.until(EC.visibility_of_element_located(AccountRegisterPage.INPUT_PASSWORD))
+    browser.find_element(*AccountRegisterPage.INPUT_PASSWORD)
+    browser.find_element(*AccountRegisterPage.YES_RADIOBUTTON)
+    browser.find_element(*AccountRegisterPage.CONTINUE_BUTTON)
