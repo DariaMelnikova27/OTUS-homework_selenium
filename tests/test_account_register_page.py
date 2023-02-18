abe7fe5d0069dc4462b599e61858d82e03378042
DariaMelnikova27 <54035863+DariaMelnikova27@users.noreply.github.com>
2023-02-18 23:06:30 +0300

    homework11

--- tests/test_account_register_page.py
+++ tests/test_account_register_page.py
@@ -12,20 +12,3 @@ def test_account_register_page(browser):
     page = AccountRegisterPage(browser)
     page.open_page(browser)
     page.check_elements_on_account_register_page()
-
-
-@allure.severity(severity_level=Severity.BLOCKER)
-@allure.feature('Регистрация нового пользователя')
-@allure.title('Регистрация нового пользователя в магазине опенкарта')
-def test_new_user_register(browser):
-    page = AccountRegisterPage(browser)
-    page.open_page(browser)
-    page.firstname_fill("Ivan") \
-        .lastname_fill("Petrov") \
-        .email_fill("123@ya.ru") \
-        .telephone_fill("12345678") \
-        .password_fill("Qwe12345") \
-        .password_confirm_fill("Qwe12345") \
-        .checkbox_agree_click() \
-        .continue_button_click()
-    assert "Your Account Has Been Created!" in page.success_register_message
--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -12,25 +12,3 @@ def test_admin_page(browser):
     page = AdminPage(browser)
     page.open_page(browser)
     page.check_elements_on_admin_page()
-
-
-@allure.severity(severity_level=Severity.NORMAL)
-@allure.feature('Действия на странице администратора')
-@allure.title('Добавление нового товара в разделе администратора')
-def test_add_new_product(browser):
-    page = AdminPage(browser)
-    page.open_page(browser)
-    page.admin_page_login()
-    page.open_add_product_section()
-    page.add_new_product()
-
-
-@allure.severity(severity_level=Severity.NORMAL)
-@allure.feature('Действия на странице администратора')
-@allure.title('Удаление товара из списка в разделе администратора')
-def test_delete_product(browser):
-    page = AdminPage(browser)
-    page.open_page(browser)
-    page.admin_page_login()
-    page.open_add_product_section()
-    page.delete_product()
